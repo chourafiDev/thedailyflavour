f722ca92c943adfd79b8a68127633574b1112a1a
chourafiDev <chourafidev@gmail.com>
2026-05-31 21:09:37 +0100

    refactor: change logo

--- src/lib/assets.ts
+++ src/lib/assets.ts
@@ -1,6 +1,6 @@
-import darkLogo from "../../public/assets/images/dark-logo.png";
+import darkLogo from "../../public/assets/images/dark-logo.svg";
 import sarahMitchell from "../../public/assets/images/sarah-mitchell.png";
 import sarahMitchellCover from "../../public/assets/images/sarah-mitchell-cover.png";
-import whiteLogo from "../../public/assets/images/white-logo.png";
+import whiteLogo from "../../public/assets/images/white-logo.svg";
 
 export { sarahMitchellCover, sarahMitchell, darkLogo, whiteLogo };
